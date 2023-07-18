46a51337f3c1b0dd5ed267e6a3a943013b9b4cb9
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-07-18 18:05:11 +0000

    last fixes

--- src/App.js
+++ src/App.js
@@ -69,7 +69,8 @@ const App = () => {
     <div>
       <form onSubmit={handleSubmit}>
         <div style={{textAlign: "center"}}>
-      <h2 style={{color:'blue'}}>Welcome to Y.D's experimental site built on GITHUB(client side) and AWS(server side)</h2>
+      <h2 style={{color:'cyan'}}>Welcome to Y.D's experimental site</h2>
+      <h2 style={{color:'darkgoldenrod'}}>built on GitHub(client side) and AWS(server side)</h2>
       </div>
         <div  style={{textAlign: "center",color:ColorContent ,display: DisplayState}}>
         <h3 style={{color:'green',display: DisplayState}}>Please enter your login details to get randomize picture:</h3>
