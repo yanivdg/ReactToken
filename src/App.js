fc2545e434a4c95695d3fd60b0c2201d42adb4b9
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-07-18 17:51:44 +0000

    final app

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ const App = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [result, setResult] = useState('');
-  const [token, setToken] = useState('');
+  const [Token, setToken] = useState('');
   const [htmlContent, setHtmlContent] = useState('');
+  const [ColorContent,setColorContent] =  useState('');
+  const [DisplayState,setDisplayState] =  useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,63 +21,67 @@ const App = () => {
         body: JSON.stringify({ username, password }),
       });
 
-      if (response.ok) {
+      if (response.ok) 
+      {
         const data = await response.json();
-        const authToken = data.token; // Assuming the token property name is "token"
-        setResult('Authentication successful');
-        setToken(authToken); // Store the token in state for further use
-
-        // Make a new request with the token
-        const htmlResponse = await fetch(url , {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ token: authToken }),
-        });
-
-        if (htmlResponse.ok) {
-          const htmlData = await htmlResponse.json();
-          setHtmlContent(htmlData.body);
-        } else {
-          setHtmlContent('Failed to load HTML content');
-        }
-      } else {
+        if(data.statusCode == 200)
+        {
+          const authToken = data.token; // Assuming the token property name is "token"
+      
+          setResult('Authentication successful');
+          setToken(authToken); // Store the token in state for further use
+          setColorContent('green');
+          setDisplayState('none');
+          // Make a new request with the token
+          const htmlResponse = await fetch(url , {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ token: authToken }),
+          });
+          if (htmlResponse.ok) 
+          {
+            const htmlData = await htmlResponse.json();
+            setHtmlContent(htmlData.body);
+          } 
+          else 
+          {
+            setHtmlContent('Failed to load HTML content');
+          }
+      } 
+      else 
+      {
         setResult('Authentication failed');
+        setColorContent('red');
         setHtmlContent('');
       }
-    } catch (error) {
+    } 
+  }
+    catch (error) {
       console.error('Error:', error);
-      setResult('An error occurred.');
+      setResult('An error occurred.'+ error);
       setHtmlContent('');
+      setColorContent('red');
     }
   };
-
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
+        <div style={{textAlign: "center"}}>
+      <h2 style={{color:'blue'}}>Welcome to Y.D's experimental site built on GITHUB(client side) and AWS(server side)</h2>
+      </div>
+        <div  style={{textAlign: "center",color:ColorContent ,display: DisplayState}}>
+        <h3 style={{color:'green',display: DisplayState}}>Please enter your login details to get randomize picture:</h3>
+          <label htmlFor="username">User Name:</label>
+          <input type="text"  id="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
           <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+         <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+         <button type="login">Login</button>
         </div>
-        <button type="submit">Submit</button>
       </form>
-      <div>{result}</div>
-      {htmlContent && <div dangerouslySetInnerHTML={{ __html: htmlContent }} />}
+      <div  style={{textAlign: "center", color: ColorContent }}>{result}</div>
+      {htmlContent && <div  style={{textAlign: "center"}} dangerouslySetInnerHTML={{ __html: htmlContent }} />}
     </div>
   );
 };
