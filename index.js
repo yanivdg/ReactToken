b1dcd8a4987de57b594d5768c278f04e7d0dd412
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-07-20 14:23:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 //import React from 'https://unpkg.com/react@18/umd/react.development.js';
 //import ReactDOM from 'react-dom/client';
 //import './index.css';
-import 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/index.css'
 //import App from './src/App';
-import App from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/App';
 //import reportWebVitals from './reportWebVitals';
-import reportWebVitals from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/reportWebVitals';
+
+//import 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/index.css'
+//import App from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/App';
+//import reportWebVitals from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
