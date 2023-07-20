b564c2442ea38260373ff99e8b1fecd6e50e4628
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-07-20 19:33:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'https://unpkg.com/react@17/umd/react.development.js';
-import ReactDOM from 'https://unpkg.com/react-dom@17/umd/react-dom.development.js';
-import App from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@abde7eb/src/App.js';
-import reportWebVitals from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/src/reportWebVitals.js';
-
+//import React from 'https://unpkg.com/react@17/umd/react.development.js';
+//import ReactDOM from 'https://unpkg.com/react-dom@17/umd/react-dom.development.js';
+//import App from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@abde7eb/src/App.js';
+//import reportWebVitals from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/src/reportWebVitals.js';
+const React = require('https://unpkg.com/react@17/umd/react.development.js');
+const ReactDOM = require('https://unpkg.com/react-dom@17/umd/react-dom.development.js');
+const App = require('https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@1d80aa7/src/App.js');
+const reportWebVitals = require('https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/src/reportWebVitals.js');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
