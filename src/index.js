a007ef4a3d929db28d6f1313cce690c996eb37a5
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-07-20 18:27:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'https://unpkg.com/react@17/umd/react.development.js';
 import ReactDOM from 'https://unpkg.com/react-dom@17/umd/react-dom.development.js';
-import App from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/src/App';
-import reportWebVitals from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/src/reportWebVitals';
+import App from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/src/App.js';
+import reportWebVitals from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/src/reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
