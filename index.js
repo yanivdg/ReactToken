eee3a1412e7df46b00bcc305b6218dfe0ebecf93
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-07-20 12:47:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import React from 'react';
+//import ReactDOM from 'react-dom/client';
+//import './index.css';
+//import App from './App';
+//import reportWebVitals from './reportWebVitals';
+import 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/index.css';
+import App from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/App';
+import reportWebVitals from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
