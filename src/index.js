d4c8a44c9a8954739959969cb2ac40ce9ad3da83
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-07-20 16:36:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom/client';
+import React from 'https://unpkg.com/react@17/umd/react.development.js';//'react';
+import ReactDOM from 'https://unpkg.com/react-dom@17/umd/react-dom.development.js';//'react-dom/client';
 //import './index.css';
 //import 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/src/index.css';
 //import App from './App';
