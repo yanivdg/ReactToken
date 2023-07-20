6e7e3d701b884e2678fdc795c47775698c5a279e
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-07-20 18:01:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom/client';
-//import './index.css';
-//import App from './App';
-//import reportWebVitals from './reportWebVitals';
 import React from 'https://unpkg.com/react@17/umd/react.development.js';
 import ReactDOM from 'https://unpkg.com/react-dom@17/umd/react-dom.development.js';
-import 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/src/index.css';
 import App from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/src/App';
 import reportWebVitals from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/src/reportWebVitals';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  React.createElement(
+    React.StrictMode,
+    null,
+    React.createElement(App, null)
+  )
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
