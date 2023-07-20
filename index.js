a306ae5fdd08f660f832ec51bd72ee9692140e5c
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-07-20 17:57:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-//import React from 'react';
-//import React from 'https://unpkg.com/react@18/umd/react.development.js';
-//import ReactDOM from 'react-dom/client';
-//import './index.css';
-//import App from './src/App';
-//import reportWebVitals from './reportWebVitals';
+import React from 'https://unpkg.com/react@17/umd/react.development.js';
+import ReactDOM from 'https://unpkg.com/react-dom@17/umd/react-dom.development.js';
+import App from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/src/App';
+import reportWebVitals from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/src/reportWebVitals';
 
-//import 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/index.css'
-//import App from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/App';
-//import reportWebVitals from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-alert('test');
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  React.createElement(
+    React.StrictMode,
+    null,
+    React.createElement(App, null)
+  )
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
