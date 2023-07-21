6f7647bfb093c28730448ce398343004b7eb67a4
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-07-21 16:34:31 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App = () => {
         {
           const authToken = data.token; // Assuming the token property name is "token"
           const fullName = data.fullname;
-          setResult('Authentication successful' + '<br\>' + 'Hello ' + fullName + '<br\>' +  'Please wait for the Image to be loaded');
+          setResult('Authentication successful' + <br\> + 'Hello ' + fullName + <br\> +  'Please wait for the Image to be loaded');
           //setToken(authToken); // Store the token in state for further use
           setColorContent('green');
           setDisplayState('none');
