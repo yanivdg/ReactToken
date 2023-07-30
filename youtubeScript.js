ab97d1b166457d15eeeb237b53ef90ba01419958
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-07-30 17:02:53 +0300

    Update youtubeScript.js

--- youtubeScript.js
+++ youtubeScript.js
@@ -1,42 +1,43 @@
-// Your YouTube Video ID
-const videoId = "c7ox2DYLdgo";
+        // Replace 'VIDEO_ID' with the YouTube video ID you want to use as the background
+    const videoId = 'c7ox2DYLdgo';
+    // Replace 'START_TIME' and 'END_TIME' with the specific time you want to loop the video (in seconds)
+    const startTime = 18;
+    const endTime = 5;
+    let player;
+    function onYouTubeIframeAPIReady() {
+      player = new YT.Player('player', {
+        height: window.innerHeight,
+        width: window.innerWidth,
+        videoId: videoId,
+        playerVars: {
+          autoplay: 1,
+          controls: 0,
+          loop: 1,
+          start: startTime,
+          end: endTime,
+          playlist: videoId,
+          modestbranding: 1,
+          fs: 0,
+          iv_load_policy: 3,
+          showinfo: 0,
+          mute: 1
+        },
+        events: {
+          onReady: onPlayerReady,
+          onStateChange: onPlayerStateChange
+        }
+      });
+    }
 
-// Video start and end times (in seconds)
-const startTime = 0;
-const endTime = 18;
+    // Function to handle video playback and loop
+    function onPlayerReady(event) {
+      event.target.playVideo();
+    }
 
-// Function to create the YouTube player
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player("player", {
-    height: "100%",
-    width: "100%",
-    videoId: videoId,
-    playerVars: {
-      autoplay: 1, // Autoplay the video
-      loop: 1, // Loop the video
-      start: startTime, // Start time of the video
-      end: endTime, // End time of the video
-      controls: 0, // Hide video controls
-      showinfo: 0, // Hide video title and info
-      modestbranding: 1, // Hide YouTube logo
-      playsinline: 1, // Play inline on iOS devices
-    },
-    events: {
-      onReady: onPlayerReady,
-      onStateChange: onPlayerStateChange,
-    },
-  });
-}
-
-// Function to handle video playback and loop
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
-
-// Function to restart the video when it reaches the end (looping)
-function onPlayerStateChange(event) {
-  if (event.data === YT.PlayerState.ENDED) {
-    player.seekTo(startTime);
-    player.playVideo();
-  }
-}
+    // Function to restart the video when it reaches the end (looping)
+    function onPlayerStateChange(event) {
+      if (event.data === YT.PlayerState.ENDED) {
+        player.seekTo(startTime);
+        player.playVideo();
+      }
+    }
