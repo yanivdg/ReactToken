db1d167e79dc8ce5f96bc1b34a0c5ffd72cf8538
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-08-08 10:11:30 +0000

    fixing code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import React from 'https://unpkg.com/react@17/umd/react.development.js';
-import ReactDOM from 'https://unpkg.com/react-dom@17/umd/react-dom.development.js';
-import App from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/src/App';
-import reportWebVitals from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/src/reportWebVitals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from 'https://cdn.jsdelivr.net/gh/yanivdg/ReactToken@gh-pages/src/App';
+import reportWebVitals from './reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,9 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'], // Add '@babel/preset-react' here
+          },
         },
       },
       {
