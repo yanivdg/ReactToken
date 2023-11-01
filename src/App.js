e34d834c84cda91fec4fcc70890e413172e44c7b
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-11-01 21:17:38 +0200

    Update App.js
    fix summary

--- src/App.js
+++ src/App.js
@@ -71,10 +71,9 @@ const App = () => {
       React.createElement('form', { onSubmit: handleSubmit },
         React.createElement('div', { style: { textAlign: 'center' } },
               React.createElement('details', { style: { textAlign: 'center' } },
-                    React.createElement('summary', { style: { textAlign: 'center' } }, 'About the Site',
+                    React.createElement('summary', { style: { textAlign: 'center' } }, 'About the Site'),
           React.createElement('h2', { style: { color: 'cyan' } }, "Welcome to Y.D's experimental site"),
           React.createElement('h2', { style: { color: 'darkgoldenrod' } }, "built on GitHub(React-client side) and AWS(python-server side)")
-                                              )
                                   )
         ),
         React.createElement('div', { style: { textAlign: 'center', color: ColorContent } },
